c17d3ec2daaaf1864009e80a0db3d33e66918b3e
walletmaker <walletmaker@gmail.com>
2016-11-12 21:45:03 -0600

    Removed template comments

--- src/javaapplication9/JavaApplication9.java
+++ src/javaapplication9/JavaApplication9.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication9;
 
 /**
@@ -15,7 +11,6 @@ public class JavaApplication9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("JavaApplication9 - Hello World!");
         System.out.println("Created by walletmaker");
     }
